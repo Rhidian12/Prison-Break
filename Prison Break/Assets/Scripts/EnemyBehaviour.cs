17f48825d2d3a4e0804820fb5edb4d8d71ecd89b
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-11-19 11:27:44 +0100

    made Notice a static function

--- Prison Break/Assets/Scripts/EnemyBehaviour.cs
+++ Prison Break/Assets/Scripts/EnemyBehaviour.cs
@@ -2,10 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class EnemyBehaviour
+public sealed class EnemyBehaviours
 {
-    public abstract void Initialize();
+    public static bool IsPlayerInFOV(Blackboard blackboard)
+    {
+        int detectionRadius = blackboard.GetData<int>("DetectionRadiusFOV");
+        int detectionAngleX = blackboard.GetData<int>("DetectionAngleXFOV");
+        int detectionAngleY = blackboard.GetData<int>("DetectionAngleYFOV");
+        Rigidbody rigidbodyPlayer = blackboard.GetData<Rigidbody>("RigidbodyPlayer");
 
-    public virtual void FixedUpdate() {}
-    public virtual void Update() {}
+        blackboard.ChangeData("HasPlayerBeenSpotted", false);
+
+        for (int x = -detectionAngleX; x < detectionAngleX; ++x)
+        {
+            for (int y = -detectionAngleY; y < detectionAngleY; ++y)
+            {
+                /* Raycast everything except enemies */
+                int layerMask = ~LayerMask.GetMask("Enemy");
+
+                Vector3 direction = Quaternion.AngleAxis(x, Vector3.right) * rigidbodyPlayer.transform.forward;
+                direction = Quaternion.AngleAxis(y, Vector3.up) * direction;
+
+                if (Physics.Raycast(rigidbodyPlayer.position, direction.normalized, out RaycastHit raycastHit, detectionRadius, layerMask))
+                {
+                    /* Check if the raycast hit the player */
+
+                    if (raycastHit.collider.gameObject.layer.ToString().Equals("Player"))
+                    {
+                        blackboard.ChangeData("HasPlayerBeenSpotted", true);
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
 }
